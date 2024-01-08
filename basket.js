5e0ea24db265a6f20b39ad831d4e26bf04ecab8d
3nvymoon <156023728+3nvymoon@users.noreply.github.com>
2024-01-09 00:18:08 +0300

    Add files via upload

---
+++ basket.js
@@ -0,0 +1,38 @@
+//--------------------------------------------------------------------------111
+function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+        change.target.classList.add('element-show');
+      }
+    });
+  }
+  let options = { threshold: [0.5] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('.element-animation');
+  for (let elm of elements) {
+    observer.observe(elm);
+  }
+  //--------------------------------------------------------------------------222
+  // Найти все ссылки начинающиеся на #
+const anchors = document.querySelectorAll('a[href^="#"]')
+
+// Цикл по всем ссылкам
+for(let anchor of anchors) {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault() // Предотвратить стандартное поведение ссылок
+    // Атрибут href у ссылки, если его нет то перейти к body (наверх не плавно)
+    const goto = anchor.hasAttribute('href') ? anchor.getAttribute('href') : 'body'
+    // Плавная прокрутка до элемента с id = href у ссылки
+    document.querySelector(goto).scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    })
+  })
+}
+  //--------------------------------------------------------------------------333
+  const button = document.querySelector('button');
+  const form = document.querySelector('#blablabla');
+  
+  button.addEventListener('click', () => {
+    form.classList.add('open');
+  });
